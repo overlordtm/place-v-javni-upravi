040c7323a6eeea90e5681852826d0768ae82c036
Andraz Vrhovec <andraz@koofr.net>
2023-02-23 22:53:32 +0100

    wip

--- pju/fetch.py
+++ pju/fetch.py
@@ -4,25 +4,25 @@ import json
 import html
 from . import __version__
 
-BASE_URL = 'http://www.pportal.gov.si'
+BASE_URL = "http://www.pportal.gov.si"
 
 # Create a pool manager to handle multiple requests
 http = urllib3.PoolManager()
-http.headers['User-Agent'] = f'pju/{__version__}'
+http.headers["User-Agent"] = f"pju/{__version__}"
 
 
 def fetch_payouts() -> pd.DataFrame:
-    COLS = ['date', 'employees_by_hours', 'employees', 'employees_all', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'L', 'N', 'O']
+    COLS = ["date", "employees_by_hours", "employees", "employees_all", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "L", "N", "O"]
 
     df = pd.DataFrame()
-    r = http.request('GET', f'{BASE_URL}/tipimesec.txt')
-    df = pd.DataFrame(json.loads(r.data)['aaData'], columns=COLS)
+    r = http.request("GET", f"{BASE_URL}/tipimesec.txt")
+    df = pd.DataFrame(json.loads(r.data)["aaData"], columns=COLS)
 
-    df[['employees_by_hours', 'employees', 'employees_all', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'L', 'N', 'O']] = df[['employees_by_hours', 'employees', 'employees_all', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'L', 'N', 'O']].apply(lambda col: col.str.replace('.', '').astype(int))
+    df[["employees_by_hours", "employees", "employees_all", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "L", "N", "O"]] = df[["employees_by_hours", "employees", "employees_all", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "L", "N", "O"]].apply(lambda col: col.str.replace(".", "", regex=False).astype(int))
 
-    df['month_year'] = pd.to_datetime(df['date'], format='%m.%Y').dt.to_period('M')
-    df.drop('date', axis=1, inplace=True)
-    df.set_index('month_year', inplace=True)
+    df["month_year"] = pd.to_datetime(df["date"], format="%m.%Y").dt.to_period("M")
+    df.drop("date", axis=1, inplace=True)
+    df.set_index("month_year", inplace=True)
     df.sort_index(inplace=True)
 
     return df
@@ -31,38 +31,40 @@ def fetch_payout_by_budget_user_group(year: int, month: int) -> pd.DataFrame:
     data = []
 
     for i in range(1, 24):
-        r = http.request('GET',  f'{BASE_URL}/ISPAP_{year}/{month}_{year}/POD/{i}_POD_VSOTE_VRST{month}.txt')
-        data.append(dict(list(map(lambda row: row.split(':'), r.data.decode('utf-8').splitlines()))))
+        r = http.request("GET",  f"{BASE_URL}/ISPAP_{year}/{month}_{year}/POD/{i}_POD_VSOTE_VRST{month}.txt")
+        data.append(dict(list(map(lambda row: row.split(":"), r.data.decode("utf-8").splitlines()))))
 
-    df = pd.DataFrame.from_records(data, index='POD')
+    df = pd.DataFrame.from_records(data, index="POD")
     df.index = df.index.str.strip()
-    df.index.rename('group', inplace=True)
-    df.rename(columns={'Z360': 'employees'}, inplace=True)
-    df = df.apply(lambda col: col.str.replace('.', '').astype(int), axis=1)
-    df.insert(0, 'month_year', pd.to_datetime(f'{year}-{month}').to_period('M'))
+    df.index.rename("group_id", inplace=True)
+    df.rename(columns={"Z360": "employees"}, inplace=True)
+    df = df.apply(lambda col: col.str.replace(".", "", regex=False).astype(int), axis=1)
+    df.insert(0, "month_year", pd.to_datetime(f"{year}-{month}").to_period("M"))
 
     return df
 
 def fetch_payout_by_budget_user(year: int, month: int) -> pd.DataFrame:
-    COLS = ['group', 'budget_user_id', 'budget_user_name', 'employees', 'gross_salary', 'NA1', 'C', 'NA2', 'D', 'NA3', 'E', 'NA4', 'NA5','NA6', 'I', 'NA7', 'J', 'NA8', 'O', 'NA9']
-    r = http.request('GET',  f'{BASE_URL}/ISPAP_{year}/{month}_{year}/PU/PU_POVPRECJE_{month}.txt')
+    COLS = ["group_id", "budget_user_id", "budget_user_name", "employees", "gross_salary", "NA1", "C", "NA2", "D", "NA3", "E", "NA4", "NA5","NA6", "I", "NA7", "J", "NA8", "O", "NA9"]
+    r = http.request("GET",  f"{BASE_URL}/ISPAP_{year}/{month}_{year}/PU/PU_POVPRECJE_{month}.txt")
 
-    df = pd.DataFrame(json.loads(html.unescape(r.data.decode('iso-8859-2')))['aaData'], columns=COLS)
+    df = pd.DataFrame(json.loads(html.unescape(r.data.decode("iso-8859-2")))["aaData"], columns=COLS)
 
-    df["budget_user_id"] = df["budget_user_id"].astype(int)
-    df["employees"] = df["employees"].astype(int)
-    df["gross_salary"] = df["gross_salary"].astype(int)
+    df[["group_id", "group_name"]] = df["group_id"].str.split(" ", n=1, expand=True)
+    df["budget_user_id"] = df["budget_user_id"].str.strip().astype(int)
+    df["budget_user_name"] = df["budget_user_name"].str.strip().astype(str)
+    df["employees"] = df["employees"].str.replace(".", "", regex=False).astype(int)
+    df["gross_salary"] = df["gross_salary"].str.replace(".", "", regex=False).astype(int)
 
-    df.set_index('budget_user_id', inplace=True)
-    df.insert(0, 'month_year', pd.to_datetime(f'{year}-{month}').to_period('M'))
+    df.set_index("budget_user_id", inplace=True)
+    df.insert(0, "month_year", pd.to_datetime(f"{year}-{month}").to_period("M"))
 
     return df
 
 def fetch_payouts_job_title(year: int, month: int) -> pd.DataFrame:
     COLS = ["group", "budget_user_id", "budget_user_name", "job_title_id", "job_title" ,"employees","gross_salary","C","D","E","F","I","J","O"]
-    r = http.request('GET',  f'{BASE_URL}/ISPAP_{year}/{month}_{year}/DM/DM_VSI_BRUTOPLACA{month}.txt')
+    r = http.request("GET",  f"{BASE_URL}/ISPAP_{year}/{month}_{year}/DM/DM_VSI_BRUTOPLACA{month}.txt")
 
-    df = pd.DataFrame(json.loads(html.unescape(r.data.decode('iso-8859-2')))['aaData'], columns=COLS)
-    df.insert(0, 'month_year', pd.to_datetime(f'{year}-{month}').to_period('M'))
-    df.set_index('job_title_id', inplace=True)
+    df = pd.DataFrame(json.loads(html.unescape(r.data.decode("iso-8859-2")))["aaData"], columns=COLS)
+    df.insert(0, "month_year", pd.to_datetime(f"{year}-{month}").to_period("M"))
+    df.set_index("job_title_id", inplace=True)
     return df
